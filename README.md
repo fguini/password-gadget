97dbfc121bf1dc7078b13ba71ee2998692beb711
Facundo Guini <fguini@gmail.com>
2021-07-05 20:46:31 +0200

    First basic functionality

---
+++ README.md
@@ -0,0 +1,12 @@
+# Password gadget
+One day I was tired of inspecting elements and changing the types, so... this happened.
+
+## Chrome extension
+It's a simple and basic chrome extension for the user to see the passwords stored in chrome (and showed in the input) 
+with the simplest configurations that came to my mind.
+
+## Information and assets
+-  Icon: unlock mobile phone by Rockicon from the Noun Project.
+-  Getting started: https://medium.com/dailyjs/how-to-build-your-first-chrome-extension-in-5-min-1dbe3eb94575
+-  Extension permissions: https://developer.chrome.com/docs/extensions/mv3/declare_permissions/
+-  Debugging extensions: https://developer.chrome.com/docs/extensions/mv2/tut_debugging/
---
+++ index.js
@@ -0,0 +1,63 @@
+(function passwordGadgetInit(options) {
+    var ALWAYS = 'ALWAYS', HOVER = 'HOVER', BUTTON = 'BUTTON';
+
+    options = options || {
+        type: BUTTON, // ALWAYS, HOVER, BUTTON
+        changeCursor: true, // If true the cursor over the input on HOVER and BUTTON types will change to "pointer"
+    };
+
+    var inputs = document.getElementsByTagName('input');
+    
+    for(var i = inputs.length; i--;)
+        if(inputs[i].type === 'password')
+            configureInput(inputs[i]);
+
+    function configureInput(el) {
+        if(options.type === ALWAYS)
+            showPassword(el);
+        else {    
+            if(options.changeCursor)
+                el.style = 'cursor: pointer;';
+            
+            addEvents(el);
+        }
+    }
+
+    function addEvents(el) {
+        var currentMouseEnter = el.onmouseenter;
+        el.onmouseenter = function onMouseEnter(e) {
+            var enterAction = options.type === HOVER
+                ? showPassword
+                : showBox;
+
+            enterAction(el);
+            if(currentMouseEnter) currentMouseEnter(e);
+        }
+
+        var currentMouseLeave = el.onmouseleave;
+        el.onmouseleave = function onMouseLeave(e) {
+            var leaveAction = options.type === HOVER
+                ? hidePassword
+                : hideBox;
+                
+            leaveAction(el);
+            if(currentMouseLeave) currentMouseLeave(e);
+        }
+    }
+
+    function showBox(el) {
+        showPassword(el);
+    }
+
+    function hideBox(el) {
+        hidePassword(el);
+    }
+
+    function showPassword(el) {
+        el.type = 'text';
+    }
+
+    function hidePassword(el) {
+        el.type = 'password';
+    }
+})(); // TODO get options
